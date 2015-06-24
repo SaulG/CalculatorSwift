b888f685ca36a6d5952f8475269aee13d22bd612
Saul Gausin <saul.gausin@gmail.com>
2015-06-23 19:20:03 -0500

    Add a variable program that return a property list

--- calculadora/CalculatorBrain.swift
+++ calculadora/CalculatorBrain.swift
@@ -46,6 +46,30 @@ class CalculatorBrain
         learnOp(Op.BinaryOperation("−") { $1 - $0 })
     }
     
+    //alias for anyobject
+    typealias PropertyList = AnyObject
+    
+    //add a property list
+    var program: PropertyList {
+        get {
+            return opStack.map { $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    
+    }
+    
     private func evaluate(ops: [Op]) -> (result:Double?, remainingOps: [Op])
     {
         if !ops.isEmpty {
